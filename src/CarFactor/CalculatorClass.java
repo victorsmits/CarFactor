42db62bad7c2ea8420ad5c2cf9f0151f76dcfcc2
bernard169 <bernard@tourneur.org>
2019-10-11 14:35:15 +0200

    create unit test for insurance computation / implement insuranceCalcul method

--- src/CarFactor/CalculatorClass.java
+++ src/CarFactor/CalculatorClass.java
@@ -3,9 +3,22 @@ package CarFactor;
 public class CalculatorClass {
     public CalculatorClass() {
     }
-    public int assuranceCalcul(int catalogValue, int power){
-        int assurancePrice = 0;
-        return assurancePrice;
+
+    public static double insuranceCalcul(int catalogValue, int power){
+        /**
+         * Computes the annual price of a car insurance given its catalog value and power.
+         * The formula used is approximated from a given set of cars with real insurance prices.
+         * This formula is not highly accurate and can be improved.
+         */
+        double insurancePrice = 12*((power*catalogValue/110000)+2);
+        //Cut off erratic values ; formula is (kind of) valid within a certain range of prices
+        if (insurancePrice < 240){
+            insurancePrice = 240;
+        }
+        else if (insurancePrice > 720){
+            insurancePrice = 720;
+        }
+        return insurancePrice;
     }
 
     public int taxeCalcul(){
--- tests/CarFactor/CalculatorClassTest.java
+++ tests/CarFactor/CalculatorClassTest.java
@@ -6,15 +6,28 @@ import org.junit.jupiter.api.Test;
 class CalculatorClassTest {
 
   @Test
-  void assuranceCalcul() {
-    int power = 25;
-    int catalogueValue = 5000;
-    int assurancePrice = 54;
-    assertEquals(assurancePrice, assuranceCalcul(catalogueValue, power));
+  void insuranceCalcul() {
+    /*
+    Current insurance function is not highly accurate, and just gives an approximation
+      of real insurance price; a relative error of maximum 45% is allowed on the result
+      (Average precision on a given set of cars with given assurance prices).
+     */
+    int power = 155;
+    int catalogueValue = 70855;
+    int expectedPrice = 54*12; //expected assurance cost
+    double admittedMargin = 0.55; // (margin is 1 - 0.45)
+    double effectivePrice = CalculatorClass.insuranceCalcul(catalogueValue, power);
+    if (effectivePrice > expectedPrice){
+      assertTrue((expectedPrice/effectivePrice) >= admittedMargin);
+    }
+    else{
+      assertTrue((effectivePrice/expectedPrice) >= admittedMargin);
+    }
   }
 
   @Test
   void taxeCalcul() {
+
   }
 
   @Test
