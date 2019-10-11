6e1e1c97b7de090cbd21c28dfeffa571cd06631b
bernard169 <bernard@tourneur.org>
2019-10-11 17:06:13 +0200

    unit tests for calculator class done

--- src/CarFactor/CalculatorClass.java
+++ src/CarFactor/CalculatorClass.java
@@ -6,32 +6,54 @@ public class CalculatorClass {
 
     public static double insuranceCalcul(int catalogValue, int power){
         /**
-         * Computes the annual price of a car insurance given its catalog value and power.
+         * Returns the annual price of a car insurance given its catalog value and power.
          * The formula used is approximated from a given set of cars with real insurance prices.
          * This formula is not highly accurate and can be improved.
+         * Arguments must be positive integers, otherwise an IllegalArgumentException will be
+         *  thrown.
          */
+        if (catalogValue < 0 || power < 0){
+            throw new IllegalArgumentException();
+        }
         double insurancePrice = 12*((power*catalogValue/110000)+2);
         //Cut off erratic values ; formula is (kind of) valid within a certain range of prices
         if (insurancePrice < 240){
             insurancePrice = 240;
         }
-        else if (insurancePrice > 720){
-            insurancePrice = 720;
+        else if (insurancePrice > 850){
+            insurancePrice = 850;
         }
         return insurancePrice;
     }
 
-    public int taxeCalcul(){
+    public static double taxCalcul(double power, double emission){
+        /**
+         * Circulation tax in Belgium per year
+         * PF [CV] = (CO2 : 45) [g/km] + (P : 40)1,6 [kW]
+         */
         int taxePrice = 0;
         return taxePrice;
     }
 
-    public int energyConsumptionCalcul(){
-        int energyConsumption = 0;
+    public static double energyConsumptionCalcul(String fuel, double consumption){
+        /**
+         * Returns an energetic consumption of a car for a given fuel, over 100km.
+         * Fuel is a either "essence", "diesel" or "electric".
+         * Consumption must be a positive number.
+         */
+        double energyConsumption = 0;
         return energyConsumption;
     }
 
-    public int annualConsumptionCalcul(){
+    public static int annualCostCalcul(int catalogValue, int power, double emission, String fuel, double consumption){
+        /**
+         * Returns the annual cost of the car for an average citizen.
+         * All arguments must be positive ; fuel is either "essence", "diesel" or "electric".
+         * Average mileage per year in Belgium is 15151km (data from 2015)
+         * Price for : - essence is 1.479 €/L
+         *             - diesel is xx
+         *             - electricity is xx /kWh
+         */
         int annualConsumption = 0;
         return annualConsumption;
     }
--- tests/CarFactor/CalculatorClassTest.java
+++ tests/CarFactor/CalculatorClassTest.java
@@ -10,31 +10,56 @@ class CalculatorClassTest {
     /*
     Current insurance function is not highly accurate, and just gives an approximation
       of real insurance price; a relative error of maximum 45% is allowed on the result
-      (Average precision on a given set of cars with given assurance prices).
+      (Maximum error on a given set of cars with given assurance prices).
      */
+    //real values taken from the given set of cars
     int power = 155;
     int catalogueValue = 70855;
     int expectedPrice = 54*12; //expected assurance cost
     double admittedMargin = 0.55; // (margin is 1 - 0.45)
     double effectivePrice = CalculatorClass.insuranceCalcul(catalogueValue, power);
     if (effectivePrice > expectedPrice){
-      assertTrue((expectedPrice/effectivePrice) >= admittedMargin);
+      assertTrue((expectedPrice/effectivePrice) >= admittedMargin,
+                  "Invalid formula for insurance computation.");
     }
     else{
-      assertTrue((effectivePrice/expectedPrice) >= admittedMargin);
+      assertTrue((effectivePrice/expectedPrice) >= admittedMargin,
+                  "Invalid formula for insurance computation.");
     }
   }
 
   @Test
-  void taxeCalcul() {
-
+  void taxCalcul() {
+    /* test based on values given here :
+      https://www.mon-assurance-auto.be/taxes/taxe-circulation-auto-belgique.html
+     */
+    double[][] realValues = {{83.56,40, 70}, {151.14, 90, 90}, {437.45, 150, 100},
+                          {637.96, 170, 120}, {1098.50, 190, 140},
+                          {1618.58, 210, 140}, {2720.8, 270, 150}};
+    for (int i = 0; i < realValues.length; i++){
+      assertEquals(realValues[i][0], CalculatorClass.taxCalcul(realValues[i][1], realValues[i][2]),
+                    "Inadequate tax computation formula.");
+    }
   }
 
   @Test
   void energyConsumptionCalcul() {
+    assertEquals(9, CalculatorClass.energyConsumptionCalcul("essence", 1),
+                  "");
+    assertEquals(12.9, CalculatorClass.energyConsumptionCalcul("diesel", 1));
+    assertEquals(1, CalculatorClass.energyConsumptionCalcul("electric", 1));
   }
 
   @Test
-  void annualConsumptionCalcul() {
+  void annualCostCalcul() {
+    double taxCost = 537.9;
+    double insuranceCost = 850; //TO BE REFACTORED : value depends on an unstable formula!
+    double gasCost = 1053.191463;
+    double totalCost = taxCost + insuranceCost + gasCost;
+    assertEquals(totalCost, CalculatorClass.annualCostCalcul(70855,
+                                                                    155,
+                                                                  114,
+                                                                "essence",
+                                                              4.7));
   }
 }
