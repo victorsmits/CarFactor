07db8bc45be6f898e4bca29e4520026e4e001ef8
Maxime <maxime.denis@hotmail.com>
2019-10-17 11:35:12 +0200

    Implement CriterionYear and CriterionYearTest

--- src/CarFactor/CriterionYear.java
+++ src/CarFactor/CriterionYear.java
@@ -2,8 +2,41 @@ package CarFactor;
 
 public class CriterionYear extends Criterion {
 
-  CriterionYear(int yearMin, int yearMax){
+  private static int yearMin;
+  private static int yearMax;
 
+  CriterionYear(int yearMi){
+    yearMin = yearMi;
+    yearMax = 0;
   }
 
+  CriterionYear(int yearMi, int yearMa){
+    yearMin = yearMi;
+    yearMax = yearMa;
+  }
+
+  public static boolean isValid(CarModelClass car){
+    if (car.getYear() <= yearMin)
+      return false;
+    else (yearMax != 0 && car.getYear() <= yearMax){
+      return true;
+    }
+    return false;
+  }
+
+  public void setYearMin(int year){
+    yearMin = year;
+  }
+
+  public int getYearMin() {
+    return yearMin;
+  }
+
+  public void setYearMax(int year) {
+    yearMax = year;
+  }
+
+  public int getYearMax() {
+    return yearMax;
+  }
 }
