238935fdb836738172877b102911cba318d49a69
El Haddadi Yassine <36259631+16146@users.noreply.github.com>
2019-11-21 10:42:05 +0100

    Last PMD Warning Fix

--- src/main/java/CarFactor/ComparatorClass.java
+++ src/main/java/CarFactor/ComparatorClass.java
@@ -59,11 +59,13 @@ public class ComparatorClass {
   /**Fix PMD : Avoid unused method parameters such as 'chain'.
    * private List<String> cutString(String chain){
    **/
-  private List<String> cutString (){
-        List<String> listCriteria = new ArrayList<String>();
-        return listCriteria;
-    }
 
+  /** Fix PMD : Avoid unused private methods such as 'cutString()'.
+   * private List<String> cutString (){
+  *      List<String> listCriteria = new ArrayList<String>();
+  *      return listCriteria;
+  *  }
+  */
     public List<CarClass> comparator(String chain){
         List<CarClass> carList = new ArrayList<CarClass>();
 
